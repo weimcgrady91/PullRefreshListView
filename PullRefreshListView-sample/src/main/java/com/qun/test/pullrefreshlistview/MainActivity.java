fd44ee0e2d20183a738de7247ffc585598368181
weiqun <331855608@qq.com>
2018-04-01 18:27:16 +0800

    finish

--- PullRefreshListView-sample/src/main/java/com/qun/test/pullrefreshlistview/MainActivity.java
+++ PullRefreshListView-sample/src/main/java/com/qun/test/pullrefreshlistview/MainActivity.java
@@ -1,11 +1,14 @@
 package com.qun.test.pullrefreshlistview;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import com.qun.lib.pullrefreshlistview.PullRefreshListView;
 
@@ -13,26 +16,113 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private PullRefreshListView mListView;
+    private ArrayAdapter<String> mAdapter;
+    private List<String> mData;
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 1:
+                    mData.addAll(0, getNewData());
+                    mListView.onRefreshCompleted();
+                    mAdapter.notifyDataSetChanged();
+                    break;
+                case 2:
+                    mData.addAll(getMoreData());
+                    if (newIndex == 12) {
+                        mListView.onLoadMoreCompleted(true);
+                    } else {
+
+                        mListView.onLoadMoreCompleted(false);
+                    }
+                    mAdapter.notifyDataSetChanged();
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        PullRefreshListView listView = findViewById(R.id.lv);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+        mData = getData();
+        mListView = findViewById(R.id.lv);
+        mListView.setOnItemClickListener(new PullRefreshListView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.e("weiqun12345","position=" + position);
+                Toast.makeText(getApplicationContext(), "position=" + position, Toast.LENGTH_SHORT).show();
             }
         });
-        List<String> data = getData();
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
-        listView.setAdapter(adapter);
+
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mData);
+        mListView.setAdapter(mAdapter);
+        mListView.setOnFetchDataListener(new PullRefreshListView.OnFetchDataListener() {
+            @Override
+            public void onRefresh() {
+                refreshData();
+            }
+
+            @Override
+            public void onLoadMore() {
+                loadMoreData();
+            }
+        });
+        mListView.setOpenPullRefresh(true);
+        mListView.setOpenLoadMore(false);
+    }
+
+    private void loadMoreData() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                    mHandler.sendEmptyMessage(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void refreshData() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(4000);
+                    mHandler.sendEmptyMessage(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private List<String> getNewData() {
+        List<String> data = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            data.add("这是刷新的第" + i + "个条目");
+        }
+        return data;
+    }
+
+    int newIndex;
+
+    private List<String> getMoreData() {
+        List<String> data = new ArrayList<>();
+        for (int i = 0; i < 1; i++) {
+            data.add("这是加载的新数据" + newIndex++);
+        }
+        return data;
     }
 
     private List<String> getData() {
         List<String> data = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 20; i++) {
             data.add("这是第" + i + "个条目");
         }
         return data;
--- pullrefreshlistview/src/main/java/com/qun/lib/pullrefreshlistview/PullRefreshListView.java
+++ pullrefreshlistview/src/main/java/com/qun/lib/pullrefreshlistview/PullRefreshListView.java
@@ -3,11 +3,20 @@ package com.qun.lib.pullrefreshlistview;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.RotateAnimation;
 import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * Created by Administrator on 2018/3/30 0030.
@@ -15,13 +24,26 @@ import android.widget.ListView;
 
 public class PullRefreshListView extends ListView {
 
+    private long mLastRefreshTime;
     private Context mContext;
     private int mHeaderViewHeight;
-    private STATE mSTATE = STATE.IDLE;
-    private int mFirstVisibleItem;
+    private boolean mIsSliderTop;
     private View mHeaderView;
+    private RefreshState mState = RefreshState.IDLE;
+    private TextView mTvState;
+    private OnFetchDataListener mOnFetchDataListener;
+    private ImageView mIvState;
+    private ProgressBar mPb;
+    private TextView mTvLastRefreshTime;
+    private View mFooterView;
+    private boolean mIsSliderBottom;
+    private TextView mTvLoadMoreState;
+    private boolean mNoMoreData;
+    private boolean mLoadingMoreData;
+    private boolean mOpenLoadMore = true;
+    private boolean mOpenPullRefresh = true;
 
-    private enum STATE {
+    private enum RefreshState {
         LOADING, READY, IDLE
     }
 
@@ -41,6 +63,7 @@ public class PullRefreshListView extends ListView {
         super(context, attrs, defStyleAttr, defStyleRes);
         mContext = context;
         initHeaderView();
+        initFooterView();
         setOnScrollListener(new OnScrollListener() {
             @Override
             public void onScrollStateChanged(AbsListView view, int scrollState) {
@@ -49,87 +72,252 @@ public class PullRefreshListView extends ListView {
 
             @Override
             public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-                mFirstVisibleItem = firstVisibleItem;
+                if (firstVisibleItem == 0) {
+                    mIsSliderTop = true;
+                } else {
+                    mIsSliderTop = false;
+                }
+                if (firstVisibleItem + visibleItemCount >= totalItemCount) {
+                    mIsSliderBottom = true;
+                } else {
+                    mIsSliderBottom = false;
+                }
             }
         });
+        setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (mOnItemClickListener != null) {
+                    mOnItemClickListener.onItemClick(parent, view, position - 1, id);
+                }
+            }
+        });
+        setSelector(android.R.color.transparent);
     }
+
+    public interface OnItemClickListener {
+        void onItemClick(AdapterView<?> parent, View view, int position, long id);
+    }
+
+    public void setOpenLoadMore(boolean openLoadMore) {
+        mOpenLoadMore = openLoadMore;
+    }
+
+    public void setOpenPullRefresh(boolean openPullRefresh) {
+        mOpenPullRefresh = openPullRefresh;
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        mOnItemClickListener = onItemClickListener;
+    }
+
+    private OnItemClickListener mOnItemClickListener;
 
     private void initHeaderView() {
         mHeaderView = LayoutInflater.from(mContext).inflate(R.layout.listview_header, null, false);
         mHeaderView.measure(0, 0);
         mHeaderViewHeight = mHeaderView.getMeasuredHeight();
-        openHeaderView(0);
-        addHeaderView(mHeaderView, null, false);
+        mHeaderView.setPadding(0, -mHeaderViewHeight, 0, 0);
+        addHeaderView(mHeaderView, null, true);
+        mTvState = mHeaderView.findViewById(R.id.tv_state);
+        mTvState.setText(R.string.pull_to_refresh);
+        mIvState = findViewById(R.id.iv_state);
+        mPb = mHeaderView.findViewById(R.id.pb);
+        mTvLastRefreshTime = findViewById(R.id.tv_last_refresh_time);
     }
 
-    /**
-     * @param dy 垂直方向拖动距离
-     */
-    private void openHeaderView(int dy) {
-        mHeaderView.setPadding(0, -mHeaderViewHeight + dy, 0, 0);
+    private void initFooterView() {
+        mFooterView = LayoutInflater.from(mContext).inflate(R.layout.listview_footer, null, false);
+        mFooterView.measure(0, 0);
+        mTvLoadMoreState = mFooterView.findViewById(R.id.tv_load_more_state);
+        mTvLoadMoreState.setText(R.string.load_more_data);
+        addFooterView(mFooterView, null, false);
+        mFooterView.setVisibility(View.GONE);
+        mFooterView.setPadding(0, -mFooterView.getMeasuredHeight(), 0, 0);
     }
 
-
-    
-    private void closeHeaderView(STATE state) {
-        int endValue;
-        if (state == STATE.READY) {
-            endValue = 0;
-        } else {
-            endValue = -mHeaderViewHeight;
-        }
-        ValueAnimator va = ValueAnimator.ofInt(mHeaderView.getPaddingTop(), endValue);
-        va.setDuration(300);
-        va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                int dy = (int) animation.getAnimatedValue();
-                mHeaderView.setPadding(0, dy, 0, 0);
-            }
-        });
-        va.start();
-    }
-
-    private void changeState(STATE state) {
-        switch (state) {
-            case LOADING:
-                break;
-            case READY:
-                break;
-            case IDLE:
-                closeHeaderView(STATE.IDLE);
-                break;
-        }
-    }
-
+    private boolean intercept;
     private int mLastY;
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
         switch (ev.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                if (mSTATE != STATE.IDLE) {
-                    return false;
-                }
                 mLastY = (int) ev.getY();
+                if (mLastRefreshTime == 0) {
+                    mTvLastRefreshTime.setText(String.format(mContext.getString(R.string.last_refresh_time), "unKnow"));
+                } else {
+                    mTvLastRefreshTime.setText(String.format(mContext.getString(R.string.last_refresh_time),
+                            formatTime(mLastRefreshTime)));
+                }
                 break;
             case MotionEvent.ACTION_MOVE:
-                if (mFirstVisibleItem != 0) {
-                    break;
-                }
                 int curY = (int) ev.getY();
                 int dy = curY - mLastY;
-                openHeaderView(dy / 2);
-                if (mHeaderView.getPaddingTop() >= 0) {
-                    mSTATE = STATE.READY;
-                } else {
-                    mSTATE = STATE.IDLE;
+                mLastY = curY;
+                if (mState == RefreshState.LOADING) {
+                    break;
+                }
+                if (mLoadingMoreData) {
+                    break;
+                }
+                if (mIsSliderTop) {
+                    if (mHeaderView.getPaddingTop() <= -mHeaderViewHeight && dy < 0) {
+                        intercept = false;
+                        if (!mOpenLoadMore) {
+                            break;
+                        }
+                        if (mIsSliderBottom && !mNoMoreData) {
+                            mFooterView.setVisibility(View.VISIBLE);
+                            mFooterView.setPadding(0, 0, 0, 0);
+                            mLoadingMoreData = true;
+                            if (mOnFetchDataListener != null) {
+                                mOnFetchDataListener.onLoadMore();
+                            }
+                            break;
+                        }
+                        break;
+                    }
+                    if (!mOpenPullRefresh) {
+                        break;
+                    }
+                    mHeaderView.setPadding(0, mHeaderView.getPaddingTop() + dy / 2, 0, 0);
+                    if (mHeaderView.getPaddingTop() >= 0) {
+                        if (mState != RefreshState.READY) {
+                            mState = RefreshState.READY;
+                            startReadyAnimation();
+                            mTvState.setText(R.string.release_to_refresh);
+                        }
+                    } else {
+                        if (mState != RefreshState.IDLE) {
+                            mState = RefreshState.IDLE;
+                            startIdleAnimation();
+                            mTvState.setText(R.string.pull_to_refresh);
+                        }
+                    }
+                    intercept = true;
+                    return true;
+                }
+                if (mIsSliderBottom && dy < 0) {
+                    if (!mOpenLoadMore) {
+                        break;
+                    }
+                    if (!mNoMoreData) {
+                        if (mState == RefreshState.LOADING) {
+                            break;
+                        }
+                        if (mLoadingMoreData) {
+                            break;
+                        }
+                        mTvLoadMoreState.setText(R.string.load_more_data);
+                        mFooterView.setVisibility(View.VISIBLE);
+                        mFooterView.setPadding(0, 0, 0, 0);
+                        mLoadingMoreData = true;
+                        if (mOnFetchDataListener != null) {
+                            mOnFetchDataListener.onLoadMore();
+                        }
+                    }
                 }
                 break;
+
             case MotionEvent.ACTION_UP:
-                closeHeaderView(mSTATE);
+                if (intercept) {
+                    confirmFinalState();
+                    return true;
+                }
                 break;
         }
         return super.onTouchEvent(ev);
+    }
+
+    private void startReadyAnimation() {
+        RotateAnimation rotateAnimation = new RotateAnimation(0, 180, RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+        rotateAnimation.setDuration(200);
+        rotateAnimation.setFillAfter(true);
+        mIvState.startAnimation(rotateAnimation);
+    }
+
+    private void startIdleAnimation() {
+        RotateAnimation rotateAnimation = new RotateAnimation(180, 0, RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+        rotateAnimation.setDuration(200);
+        rotateAnimation.setFillAfter(true);
+        mIvState.startAnimation(rotateAnimation);
+    }
+
+    private void confirmFinalState() {
+        intercept = false;
+        switch (mState) {
+            case IDLE:
+                foldHeaderView(mHeaderView.getPaddingTop(), -mHeaderViewHeight);
+                break;
+            case READY:
+                foldHeaderView(mHeaderView.getPaddingTop(), 0);
+                mState = RefreshState.LOADING;
+                mTvState.setText(R.string.refreshing);
+                mPb.setVisibility(View.VISIBLE);
+                mIvState.clearAnimation();
+                mIvState.setVisibility(View.INVISIBLE);
+                mLastRefreshTime = System.currentTimeMillis();
+                if (mOnFetchDataListener != null) {
+                    mOnFetchDataListener.onRefresh();
+                }
+                break;
+            case LOADING:
+                break;
+        }
+    }
+
+    private void foldHeaderView(int fromY, int toY) {
+        ValueAnimator animator = ValueAnimator.ofInt(fromY, toY);
+        animator.setDuration(300);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                mHeaderView.setPadding(0, (int) animation.getAnimatedValue(), 0, 0);
+            }
+        });
+        animator.start();
+    }
+
+    public void onRefreshCompleted() {
+        mTvState.setText(R.string.refresh_finish);
+        foldHeaderView(0, -mHeaderViewHeight);
+        mState = RefreshState.IDLE;
+        mPb.setVisibility(View.GONE);
+        mIvState.setVisibility(View.VISIBLE);
+    }
+
+    public void onLoadMoreCompleted(boolean hasMore) {
+        mNoMoreData = hasMore;
+        mLoadingMoreData = false;
+        if (mNoMoreData) {
+            mTvLoadMoreState.setText(R.string.no_more_data);
+            mFooterView.setVisibility(View.VISIBLE);
+            mFooterView.setPadding(0, 0, 0, 0);
+        } else {
+            mFooterView.setVisibility(View.GONE);
+            mFooterView.setPadding(0, -mFooterView.getMeasuredHeight(), 0, 0);
+        }
+
+    }
+
+    public void setOnFetchDataListener(OnFetchDataListener onFetchDataListener) {
+        mOnFetchDataListener = onFetchDataListener;
+    }
+
+
+    public interface OnFetchDataListener {
+        void onRefresh();
+
+        void onLoadMore();
+    }
+
+    private String formatTime(long time) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(time);
+        return sdf.format(calendar.getTime());
     }
 }
